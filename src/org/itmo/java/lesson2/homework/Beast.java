3dd25b8ccb66e23a5346cec46a8a229811275131
ezulex <bochik1989@mail.ru>
2022-06-21 21:30:01 +0300

    Beast

---
+++ src/org/itmo/java/lesson2/homework/Beast.java
@@ -0,0 +1,50 @@
+package org.itmo.java.lesson2.homework;
+
+public class Beast {
+    private String name;
+    private String kind;
+
+    public Beast() {
+    }
+
+    public Beast(String name, String kind) {
+        this.name = name;
+        this.kind = kind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    @Override
+    public String toString() {
+        return "Beast{" +
+                "name='" + name + '\'' +
+                ", kind='" + kind + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Beast beast = new Beast();
+        Beast beast1 = new Beast("Корова", "Млекопитающее");
+
+        System.out.println(beast1.getName());
+        System.out.println(beast1.getKind());
+
+        System.out.println(beast);
+        beast.setName("Кот");
+        System.out.println(beast);
+    }
+}
