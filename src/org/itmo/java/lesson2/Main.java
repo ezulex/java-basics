8e1a15a4c9ece334985f469133416aa31d443060
ezulex <bochik1989@mail.ru>
2022-06-21 20:30:03 +0300

    Use get/set

--- src/org/itmo/java/lesson2/Main.java
+++ src/org/itmo/java/lesson2/Main.java
@@ -3,7 +3,18 @@ package org.itmo.java.lesson2;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("BMW", "X5", Color.BLUE, true);
+
         System.out.println(car);
+
+        car.setSeatPlace(5);
+        car.setPrice(1000000);
+        car.setEngineCapacity(5.0);
+
+        System.out.println(car);
+
+        car.setColor(Color.GREEN);
+
+        System.out.println(car.getColor());
     }
 }
 
