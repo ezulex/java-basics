094a89b1fd623ad2aa4679d49115c4e0ab561287
ezulex <bochik1989@mail.ru>
2022-06-21 20:23:08 +0300

    toString And getter/setter

--- src/org/itmo/java/lesson2/Car.java
+++ src/org/itmo/java/lesson2/Car.java
@@ -15,4 +15,73 @@ public class Car {
         this.color = color;
         this.isNew = isNew;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", seatPlace=" + seatPlace +
+                ", color=" + color +
+                ", price=" + price +
+                ", engineCapacity=" + engineCapacity +
+                ", isNew=" + isNew +
+                '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeatPlace() {
+        return seatPlace;
+    }
+
+    public void setSeatPlace(int seatPlace) {
+        this.seatPlace = seatPlace;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
 }
