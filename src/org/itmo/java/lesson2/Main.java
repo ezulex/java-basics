7d62215483aab5d4952e1d39905bb25cd192f1fa
ezulex <bochik1989@mail.ru>
2022-06-21 20:39:09 +0300

    totalPrice

--- src/org/itmo/java/lesson2/Main.java
+++ src/org/itmo/java/lesson2/Main.java
@@ -4,17 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("BMW", "X5", Color.BLUE, true);
 
-        System.out.println(car);
+        //System.out.println(car);
 
         car.setSeatPlace(5);
         car.setPrice(1000000);
         car.setEngineCapacity(5.0);
 
-        System.out.println(car);
+        //System.out.println(car);
 
         car.setColor(Color.GREEN);
 
-        System.out.println(car.getColor());
+        //System.out.println(car.getColor());
+
+        long totalPrice = CarService.totalPrice(car, 250000);
+        System.out.println(totalPrice);
     }
 }
 
