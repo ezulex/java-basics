fd8475a4efab18573adfbd0beea886377600d699
ezulex <bochik1989@mail.ru>
2022-06-21 20:14:49 +0300

    Add Car and Color

---
+++ src/org/itmo/java/lesson2/Car.java
@@ -0,0 +1,11 @@
+package org.itmo.java.lesson2;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int seatPlace;
+    private Color color;
+    private long price;
+    private double engineCapacity;
+    private boolean isNew;
+}
---
+++ src/org/itmo/java/lesson2/Main.java
@@ -0,0 +1,9 @@
+package org.itmo.java.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car);
+    }
+}
+
