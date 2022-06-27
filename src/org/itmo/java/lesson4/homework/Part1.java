18b99020abeb62cb8bd6b970e8d8c23c44d16ce6
ezulex <bochik1989@mail.ru>
2022-06-27 21:20:34 +0300

    homework4 3-4

--- src/org/itmo/java/lesson4/homework/Part1.java
+++ src/org/itmo/java/lesson4/homework/Part1.java
@@ -1,16 +1,21 @@
 package org.itmo.java.lesson4.homework;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Part1 {
-    public static int returnOdd() {
+    public static void returnOdd() {
         for (int i = 0; i < 100; i++) {
             if(i % 2 != 0) {
                 System.out.println(i);
             }
         }
-        return 1;
     }
 
-    public static int divThree() {
+    public static void divThree() {
         for (int i = 0; i < 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println(i + " Делится на 3 и на 5");
@@ -20,14 +25,58 @@ public class Part1 {
                 System.out.println(i + " Делится на 3");
             }
         }
-        return 1;
     }
 
+    public static boolean checkSumTwo() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите первое число: ");
+        int a = Integer.parseInt(reader.readLine());
+
+        System.out.println("Введите второе число: ");
+        int b = Integer.parseInt(reader.readLine());
+
+        System.out.println("Введите третье число: ");
+        int c = Integer.parseInt(reader.readLine());
+
+        if((a + b) == c) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
+
+    public static boolean checkSumThree() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите первое число: ");
+        int a = Integer.parseInt(reader.readLine());
+
+        System.out.println("Введите второе число: ");
+        int b = Integer.parseInt(reader.readLine());
+
+        System.out.println("Введите третье число: ");
+        int c = Integer.parseInt(reader.readLine());
+
+        if(b > a && c > b) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+//        returnOdd();
+//        divThree();
+//        Part1.checkSumTwo();
+//        Part1.checkSumThree();
 
 
-    public static void main(String[] args) {
-        returnOdd();
-        divThree();
+
     }
 }
 
