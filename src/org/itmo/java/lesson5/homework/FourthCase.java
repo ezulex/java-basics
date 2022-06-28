2cc4c6e0e2c3b9b78397ae9bad433502efbf21b6
ezulex <bochik1989@mail.ru>
2022-06-28 15:40:49 +0300

    homework5 4

---
+++ src/org/itmo/java/lesson5/homework/FourthCase.java
@@ -0,0 +1,24 @@
+package org.itmo.java.lesson5.homework;
+
+public class FourthCase {
+
+    public static int countStringContains(String text, String containingText) {
+        int buf = -1;
+        int counter = 0;
+        for (int i = 0; i < text.length();) {
+            buf = text.indexOf(containingText, i);
+            if(buf >= 0) {
+                i = buf + 1;
+                counter++;
+            } else {
+                break;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int result = countStringContains("Это текст для проверки вхождения слова текст", "текст");
+        System.out.println(result);
+    }
+}
